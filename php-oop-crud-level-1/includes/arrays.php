131be5d6909fed5515ee2005998b40ed08fe794f
picocontrol <44052299+picocontrol@users.noreply.github.com>
2018-11-02 16:47:05 +0100

    Add files via upload
    Practicing oop to create an online marketplace

---
+++ php-oop-crud-level-1/includes/arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+// Nav Menu Items
+$navItems = array(
+    array ( 
+slug => "index.php",
+title => "Home"
+    ),
+    array ( 
+slug => "index.php",
+title => "Home"
+    ),
+    array ( 
+slug => "index.php",
+title => "Home"
+    ),
+    array ( 
+slug => "contact.php",
+title => "Contact"
+    ),
+);
+?>
